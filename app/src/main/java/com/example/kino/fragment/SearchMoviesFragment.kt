e7e5b9a5ba68571044effce36fc478785f578538
Aleksei Lobanov <lobanov782@gmail.com>
2024-07-19 03:04:10 +0300

    - Добавлен SearchView

--- app/src/main/java/com/example/kino/fragment/SearchMoviesFragment.kt
+++ app/src/main/java/com/example/kino/fragment/SearchMoviesFragment.kt
@@ -4,22 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.kino.databinding.FragmentSearchMoviesBinding
 
 class SearchMoviesFragment : Fragment() {
-	private var binding: FragmentSearchMoviesBinding? = null
+	private lateinit var binding: FragmentSearchMoviesBinding
 
 	override fun onCreateView(
 		inflater: LayoutInflater,
 		container: ViewGroup?,
 		savedInstanceState: Bundle?
-	): View? {
+	): View {
 		binding = FragmentSearchMoviesBinding.inflate(
 			inflater,
 			container,
 			false
 		)
-		return binding?.root
+		return binding.root
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+
+		binding.search.setOnSearchClickListener {
+			Toast.makeText(context, "Search box clicked!", Toast.LENGTH_SHORT).show()
+		}
 	}
 }
